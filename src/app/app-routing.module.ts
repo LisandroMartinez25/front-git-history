fe0f5f6a267c617819122248f166bc4583d2a476
Lisandro Martínez <lisann.mtz@gmail.com>
2021-03-13 01:44:01 -0600

    [feat-dev-i1]: add navbar, header and initial routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '**',
+  loadChildren: () => import('./modules/repository/repository.module').then(m => m.RepositoryModule)
+},{
+  path: 'Commits',
+  loadChildren: () => import('./modules/commits/commits.module').then(m => m.CommitsModule)
+},{
+  path: 'Profile',
+  loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule)
+},{
+  path: 'Pull-Requests',
+  loadChildren: () => import('./modules/pull-requests/pull-requests.module').then(m => m.PullRequestsModule)
+},{
+  path: 'Issues',
+  loadChildren: () => import('./modules/issues/issues.module').then(m => m.IssuesModule)
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
